5b26e280ec15f992d877a2e9ef75a4d8cc1b405d
Marco Gabriel <marco.gabriel@loxone.com>
2023-03-12 18:24:14 +0100

    Update structure and add sidebar tabs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <main v-if="true">
+    <main>
         <section ref="map__wrapper" id="content" class="map__wrapper">
             <svg ref="map__element" class="map__element" fill="#ececec" stroke="black" stroke-width=".2" width="100%" viewBox="0 0 2000 857" xmlns="http://www.w3.org/2000/svg">
                 <g @click="selectCountry(country.code, $event)" :class="country.code" :key="country.code" v-for="country in $store.state.map_data">
@@ -7,35 +7,44 @@
                 </g>
             </svg>
         </section>
-        <div v-if="statisticVisble" class="overlay__statistic">
-            <div class="overlay__statistic--header">
-                <h1>Emission pro Jahr</h1>
-                <div class="search__wrapper">
-                    <input type="text" placeholder="Name, Emission or Country" v-model="searchString">
-                    <span class="search__icon" v-if="searchString.length > 0" @click="searchString = ''">
-                        <span class="serach__icon--line"></span>
-                        <span class="serach__icon--line"></span>
-                    </span>
-                </div>
+        <section id="sidebar">
+            <div class="sidebar__header">
+                <div @click="tabIndex = 1" class="sidebar__header--tab" :class="{'active': tabIndex == 1}">1</div>
+                <div @click="tabIndex = 2" class="sidebar__header--tab" :class="{'active': tabIndex == 2}">2</div>
             </div>
-            <div class="overlay__statistic--content">
-                <div class="filtered__list--wrapper">
+            <div class="sidebar__content">
+                <div v-if="tabIndex == 1" class="sidebar__content--tab">
+                    <h1>Filter</h1>
                     <table class="filtered__list">
-                        <tr class="filtered__list--header">
-                            <td class="filtered__list--header__name">Name</td>
-                            <td class="filtered__list--header__emission">Emission</td>
-                            <td class="filtered__list--header__country">Land</td>
-                        </tr>
-                        <tr @click="focusCountry = company.country" v-for="company in filteredValues" class="filtered__list--item">
-                            <td class="filtered__list--item__name">{{company.name}}</td>
-                            <td class="filtered__list--item__emission">{{company.emission}} Mio. t</td>
-                            <td class="filtered__list--item__country">{{company.country}}</td>
-                        </tr>
+                        <thead>
+                            <tr>
+                                <td>
+                                    <input type="checkbox" :checked="focusCountry.length > 0" @click="checkOrUncheckAllCountries()">
+                                </td>
+                                <td>LAND</td>
+                                <td>Firmenname</td>
+                                <td>Emissionen</td>
+                            </tr>
+                        </thead>
+                        <tbody @click="updateFocusCountries(country.code)" :key="country.code" v-for="country in $store.state.map_data">
+                            <tr>
+                                <td rowspan="4">
+                                    <input type="checkbox" :checked="focusCountry.includes(country.code)">
+                                </td>
+                                <td rowspan="4">{{ country.code }}</td>
+                            </tr>
+                            <tr v-for="company in country.companies">
+                                <td>{{ company.name }}</td>
+                                <td>{{ company.emission }}</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
+                <div v-if="tabIndex == 2" class="sidebar__content--tab">
+                    <h1>Statistik</h1>
+                </div>
             </div>
-        </div>
-        <div @click="toggleStatistic()" class="overlay__icon"><template v-if="!statisticVisble">?</template><template v-else>X</template></div>
+        </section>
     </main>
 </template>
 
@@ -44,10 +53,9 @@ export default {
     name: 'Home',
     data() {
         return {
-            filteredValues: Object.values(this.$store.state.company_data.sort((a, b) => b.emission - a.emission)),
+            tabIndex: 1,
             selected: null,
             searchString: '',
-            statisticVisble: false,
             color_value_map: [],
             unique_emission_values: this.$store.state.map_data.map(p => p.emission).filter((value, index, self) => self.indexOf(value) === index).sort((a, b) => b - a),
             focusCountry: [],
@@ -62,25 +70,26 @@ export default {
         }
     },
     methods: {
-
-        selectCountry(code, event) {
-            console.log(event.shiftKey);
-            //IF shift key on keayboard is pressed add country to focus
-            if(event.shiftKey) {
-                if(!this.focusCountry.includes(code)) {
-                    this.focusCountry.push(code);
-                }
+        checkOrUncheckAllCountries() {
+            if(this.focusCountry.length > 0) {
+                this.focusCountry = [];
             } else {
-                this.focusCountry = [code];
-            }
-            this.searchString = 'country:'+this.focusCountry.join(',')+'';
-
-            if(!this.statisticVisble) {
-                this.toggleStatistic();
+                this.focusCountry = this.$store.state.map_data.map(p => p.code);
             }
         },
-        toggleStatistic() {
-            this.statisticVisble = !this.statisticVisble;
+        updateFocusCountries(code) {
+            console.log("updateFocusCountries", code);
+            if(this.focusCountry.includes(code)) {
+                this.focusCountry = this.focusCountry.filter(p => p != code);
+            } else {
+                this.focusCountry.push(code);
+            }
+        },
+        selectCountry(code, event) {
+            //IF shift key on keayboard is pressed add country to focus
+            if(event.shiftKey) {
+                this.updateFocusCountries(code);
+            }
         },
         getMapColor(val = false, countryCode = false) {
 
@@ -104,81 +113,8 @@ export default {
                 return 'hsl(' + h + ' 100% '+l+'%)';
             }
         },
-        dragStart(e) {
-            if(e.target.classList.value == 'map__element' && this.$refs.map__element){
-                this.selected = e.target;
-                this.mouse.x = e.clientX;
-                this.offset.x = this.$refs.map__element.getBoundingClientRect().left;
-            }
-        },
-        dragMove(e) {
-            if(this.selected){
-                let left = (this.offset.x + e.clientX - this.mouse.x - 0);
-                let wrapperWidth = this.$refs.map__wrapper.getBoundingClientRect().width
-                let elementWidth = this.$refs.map__element.getBoundingClientRect().width;
-
-                if(left > 0) {
-                    left = 0;
-                }
-                
-                if(left < (wrapperWidth - elementWidth)) {
-                    left = wrapperWidth - elementWidth;
-                }
-                
-                this.selected.style.marginLeft = left + "px";
-            }
-        },
-        dragEnd(e) {
-            this.selected = null;
-        }
-    },
-    watch: {
-        searchString: function (val) {
-            if(val.startsWith("country:")) {
-                let searchString = val.split(":")[1];
-                this.filteredValues = Object.values(this.$store.state.company_data.filter(p => p.country.toLowerCase() == searchString.toLowerCase())).sort((a, b) => b.emission - a.emission);
-                this.focusCountry = this.filteredValues.map(p => p.country);
-
-                return;
-            } else if(val.startsWith("name:")) {
-                let searchString = val.split(":")[1];
-                this.filteredValues = Object.values(this.$store.state.company_data.filter(p => p.name.toLowerCase() == searchString.toLowerCase())).sort((a, b) => b.emission - a.emission);
-                this.focusCountry = this.filteredValues.map(p => p.country);
-                return;
-            } else if(val.startsWith("emission:") || val.startsWith("emissions:")) {
-                let searchString = val.split(":")[1];
-                this.filteredValues = Object.values(this.$store.state.company_data.filter(p => p.emission.toString() == searchString.toLowerCase())).sort((a, b) => b.emission - a.emission);
-                this.focusCountry = this.filteredValues.map(p => p.country);
-                return;
-            } else if(val.length >= 0) {
-                this.filteredValues = Object.values(this.$store.state.company_data.filter(p => p.name.toLowerCase().includes(val.toLowerCase()) || p.country.toLowerCase().includes(val.toLowerCase()) || p.emission.toString().includes(val.toLowerCase()))).sort((a, b) => b.emission - a.emission);
-                this.focusCountry = this.filteredValues.map(p => p.country);
-            } else {
-                this.filteredValues = Object.values(this.$store.state.company_data.sort((a, b) => b.emission - a.emission));
-            }
-        }
     },
     created() {
-        document.addEventListener("mousedown", e => {
-            this.dragStart(e);
-        });
-        document.addEventListener("touchstart", e => {
-            this.dragStart(e);
-        });
-
-        document.addEventListener("mousemove", e => {
-            this.dragMove(e);
-        });
-        document.addEventListener("touchmove", e => {
-            this.dragMove(e);
-        });
-
-        document.addEventListener("mouseup", e => {
-            this.dragEnd(e);
-        });
-        document.addEventListener("touchend", e => {
-            this.dragEnd(e);
-        });
 
         
     },
@@ -194,42 +130,16 @@ export default {
     @import "../scss/utilities/_variables.scss";
     main{
         display: grid;
+        grid-template-columns: auto 600px;
         height: calc(100% - 48px);
-        grid-template-columns: 100%;
         position: relative;
         overflow: hidden;
     }
 
-    .overlay__statistic{
-        position: absolute;
-        right: 48px;
-        top: 132px;
-        width: 30%;
-        min-width: 300px;
-        background-color: #FFFFFF;
-        box-shadow: 0 0 10px 0 rgb(0 0 0 / 20%);
-        border-radius: 5px;
-        padding: 40px 20px 20px 20px;
-    }
-    .overlay__icon{
-        box-shadow: 0 0 10px 0 rgb(0 0 0 / 20%);
-        position: absolute;
-        right: 48px;
-        top: 48px;
-        width: 64px;
-        height: 64px;
-        background-color: #FFFFFF;
-        text-align: center;
-        line-height: 64px;
-        font-size: 32px;
-        border-radius: 50%;
-        user-select: none;
-        cursor: pointer;
-    }
-
-    #content{
+       #content{
         background-color: lightgrey;
-        overflow: hidden;
+        overflow-x: auto;
+        overflow-y: hidden;
 
         svg{
             cursor: move;
@@ -248,118 +158,45 @@ export default {
         text-align: center;
     }
 
-    .overlay__statistic--content{
+    #sidebar{
+        display: grid;
+        border-left: 2px solid black;
+        grid-template-rows: 48px auto;
         overflow: auto;
-        max-height: 50vh;
-        margin-right: -20px;
-        margin-left: -20px;
-    }
 
-    .filtered__list{
-        width: 100%;
-        border-spacing: 0;
-
-        tr{
-            cursor: pointer;
-            user-select: none;
-
-            &:hover{
-                background-color: #cecece !important;
-            }
-
-            &:nth-child(odd){
-                background-color: #ececec;
-            }
-        }
-
-        td{
-            border-bottom: 1px solid #ececec;
-            padding: 5px 2px;
-            border-collapse: collapse;
-
-            &:nth-child(1) {
-                padding-left: 20px;
-            }
-
-            &:nth-child(2) {
-                font-style: italic;
-            }
-
-            &:nth-child(3) {
+        .sidebar__header{
+            display: grid;
+            grid-template-columns: 50% 50%;
+            
+            .sidebar__header--tab{
                 text-align: center;
-                padding-right: 20px;
+                line-height: 48px;
+                background-color: #CeCECe;
+                cursor: pointer;
+                transition: all .2s ease-in-out;
+
+                &.active{
+                    background-color: #fff;
+                }
+            }
+        }
+
+        .sidebar__content{
+            padding: 20px;
+
+            .sidebar__content--tab{
+                .filtered__list{
+                    width: 100%;
+                    border: none;
+                    border-collapse: collapse;
+
+                    tbody:nth-child(odd){
+                        background-color: #f2f2f2;
+                    }
+
+                    td{}
+                }
             }
         }
     }
-
-    .filtered__list--header,
-    .filtered__list--item{
-
-        > div{
-            padding: 0 5px;
-        }
-    }
-
-    .filtered__list--header{
-        font-weight: bold;
-    }
-
-    .overlay__statistic--header{
-        input{
-            padding: 10px 20px;
-            outline: none;
-            border: none;
-            width: 100%;
-            font-size: 16px;
-            border-top: 1px solid #ececec;
-            border-bottom: 1px solid #ececec;
-            margin: 30px -20px 0 -20px;
-        }
-    }
-    .search__wrapper{
-        position: relative;
-
-        .search__icon{
-            position: absolute;
-            right: 0;
-            top: 31px;
-            display: inline-block;
-            height: 38px;
-            width: 38px;
-            padding: 5px;
-            box-sizing: border-box;
-            text-align: center;
-            cursor: pointer;
-
-            &:after,
-            &:before{
-                content: "";
-                display: inline-block;
-                height: 28px;
-                width: 1px;
-                background-color: #000000;
-                transform: rotate(45deg);
-            }
-            &:before{
-                transform: rotate(-45deg);
-            }
-        }
-    }
-
-    @media screen and (max-width: 768px) {
-        .overlay__icon{
-            top: 16px;
-            right: 16px;
-        }
-
-        .overlay__statistic{
-            left: 16px;
-            right: 16px;
-            width: auto;
-            top: 102px;
-        }
-        .overlay__statistic--content{
-            max-height: 40vh;
-        }
-    }   
 </style>
