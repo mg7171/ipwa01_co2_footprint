111739f764477dd991db759e2a684b51c2e0ccd1
Marco Gabriel <marco.gabriel@loxone.com>
2023-04-10 21:20:42 +0200

    Add about and imprint page

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <main id="about">
+        <div class="wrapper">
+            <h1>About</h1>
+            <p>Welcome to <i>ClimateGuardians</i>, a dedicated non-profit organization focused on raising awareness and promoting action against climate change. Our mission is to provide transparent and accurate information on CO2 emissions from countries and companies around the world. We believe that through knowledge and understanding, we can inspire change and encourage responsible practices that will contribute to a sustainable future for our planet.</p>
+            <p>Our team of passionate experts works tirelessly to compile and present reliable data on CO2 emissions. We are committed to fostering collaboration and engaging in meaningful conversations with stakeholders from various sectors, to drive collective action towards a greener and healthier world.</p>
+            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </p>
+            <br/>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "About",
+    };
+</script>
---
+++ src/views/Imprint.vue
@@ -0,0 +1,25 @@
+<template>
+    <main id="imprint">
+        <div class="wrapper">
+            <h1>Imprint</h1>
+            <p><b>ClimateGuardians</b><br/>
+                123 Frosty Lane<br/>
+                North Pole, NP 00001<br/>
+                Arctic Circle<br/>
+
+                Phone: <a href="tel: +000 123 456789">+000 123 456789</a><br/>
+                Email: <a href="mailto:marco@gabriel.lol">marco@gabriel.lol</a>
+            </p>
+            <p>
+                Represented by:<br/>
+                Marco Gabriel
+            </p>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "Imprint",
+    };
+</script>
