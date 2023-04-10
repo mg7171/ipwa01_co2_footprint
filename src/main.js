fe16edb27e3da9ee5927cf592335a0fa743cf3be
Marco Gabriel <marco.gabriel@loxone.com>
2023-04-10 21:19:51 +0200

    Remove tranlsations since they are not needed

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
-import i18n from './i18n/index.js'
 
 const axiosInstance = axios.create({
   baseURL: 'https://www.gabriel.lol',
@@ -11,6 +10,6 @@ const axiosInstance = axios.create({
   withCredentials: true
 });
 
-const app = createApp(App).use(i18n).use(store).use(router)
+const app = createApp(App).use(store).use(router)
 app.config.globalProperties.$axios = axiosInstance
 app.mount('#app');
